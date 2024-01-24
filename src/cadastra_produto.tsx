3baa450dfe48ab428d50b49ae9e7dc152190e424
Rapha <157547048+raphagramos@users.noreply.github.com>
2024-01-24 13:27:41 -0300

    Adicionando Dropdown de selecionar cliente na página Crediário (#4)
    * Feature/set up the project (#3)
    
    * update git ignore
    
    * remove wrapper folder
    
    * add npm run serve
    
    * add font to body root
    
    * create enum
    
    * add eslint
    
    * update package.json scripts
    
    * remove assets from public
    
    * create enums
    
    ---------
    
    Co-authored-by: Claudio Fernandes <sup-cla-007@criticaltechworks.com>
    
    * Adicionando Dropdown Crediário
    
    ---------
    
    Co-authored-by: Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
    Co-authored-by: Claudio Fernandes <sup-cla-007@criticaltechworks.com>

--- src/cadastra_produto.tsx
+++ src/cadastra_produto.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import './App.css';
-import Swal from 'sweetalert2'
-import { NumericFormat } from 'react-number-format';
+import React, { useState } from "react";
+import "./App.css";
+import Swal from "sweetalert2";
+import { NumericFormat } from "react-number-format";
+import { CadastraProdutoEnum } from "./modules/common/core/enums/cadastra_produto.enum";
 
-const CadastraProduto = () => {
+export const CadastraProduto: React.FC = () => {
   const [formData, setFormData] = useState({
-    nome_produto: '',
-    preco: '',
-    descricao: '',
+    nome_produto: "",
+    preco: "",
+    descricao: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,52 +24,51 @@ const CadastraProduto = () => {
 
     try {
       Swal.fire({
-        title: 'Carregando...',
+        title: "Carregando...",
         html: '<img src="https://media1.tenor.com/m/kXdU71jKYuYAAAAd/ameno-dorime-dorime.gif" width="300"  style="margin: 0 auto" alt="GIF">',
-        icon: 'info',
-        showConfirmButton: false
-    })    
-      const response = await fetch('http://localhost:8081/produtos', {
-        method: 'POST',
+        icon: "info",
+        showConfirmButton: false,
+      });
+      const response = await fetch("http://localhost:8081/produtos", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-
       });
 
       if (response.ok) {
         Swal.fire({
-          title: 'Sucesso!',
-          text: 'Cadastro efetuado com Sucesso!',
-          icon: 'success',
-          confirmButtonText: 'Ok!'
-      })
-        console.log('Produto cadastrado com sucesso');
+          title: "Sucesso!",
+          text: "Cadastro efetuado com Sucesso!",
+          icon: "success",
+          confirmButtonText: "Ok!",
+        });
+        console.log("Produto cadastrado com sucesso");
       } else {
-        console.error('Erro ao cadastrar Produto');
+        console.error("Erro ao cadastrar Produto");
       }
     } catch (error) {
-      console.error('Erro ao processar a requisição:', error);
+      console.error("Erro ao processar a requisição:", error);
     }
   };
 
   return (
     <div>
-      <h2>Insira os Dados do Produto</h2>
+      <h2>{CadastraProdutoEnum.INSIRA_DADOS}</h2>
       <form className="cadastro-form" onSubmit={handleSubmit}>
-        <label htmlFor="nome">Nome:</label>
+        <label htmlFor="nome">{CadastraProdutoEnum.NOME}</label>
         <input
           type="text"
           id="nome_produto"
           name="nome_produto"
-          placeholder='Leite'
+          placeholder="Leite"
           value={formData.nome_produto}
           onChange={handleChange}
         />
 
-          <label htmlFor="preco">Preço:</label>
-          <NumericFormat
+        <label htmlFor="preco">{CadastraProdutoEnum.PRECO}</label>
+        <NumericFormat
           id="preco"
           name="preco"
           placeholder="R$10,00"
@@ -80,25 +80,22 @@ const CadastraProduto = () => {
             });
           }}
           thousandSeparator={true}
-          prefix={'R$'}
+          prefix={"R$"}
           decimalScale={2}
         />
 
-        <label htmlFor="descricao">Telefone:</label>
+        <label htmlFor="descricao">{CadastraProdutoEnum.TELEFONE}</label>
         <input
           type="text"
           id="descricao"
           name="descricao"
-          placeholder='Leite Desnatado Tirol'
+          placeholder="Leite Desnatado Tirol"
           value={formData.descricao}
           onChange={handleChange}
         />
 
-
-        <button type="submit">Cadastrar</button>
+        <button type="submit">{CadastraProdutoEnum.CADASTRAR}</button>
       </form>
     </div>
   );
 };
-
-export default CadastraProduto;
---
+++ src/modules/common/core/enums/padaria.enum.ts
@@ -0,0 +1,11 @@
+export enum PadariaEnum {
+  TITLE = "Padaria Vó Mercides",
+}
+
+export enum PadariaTabsEnum {
+  CLIENTES = "Clientes",
+  CREDIARIO = "Crediário",
+  PRODUTOS = "Produtos",
+  CADASTRAR_CLIENTE = "Cadastrar Cliente",
+  CADASTRAR_PRODUTO = "Cadastrar Produto",
+}
